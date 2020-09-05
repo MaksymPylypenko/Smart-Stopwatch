395d3ea271af89c974b18752e11c213c6caf76a0
Maksym <maximka3d@gmail.com>
2020-09-04 21:02:05 -0500

    added login & logout routine

--- mysite/stopwatch/models.py
+++ mysite/stopwatch/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from django.conf import settings   
+User = settings.AUTH_USER_MODEL 
      
-class Record(models.Model):    
-    record_date = models.DateField(blank=True)
+class Record(models.Model):     
+    user = models.ForeignKey(User, default = 1, null = True, on_delete = models.SET_NULL) 
     start_time = models.TimeField(blank=True)
-    end_time = models.TimeField(blank=True)
-    description = models.CharField(max_length=200)
+    duration = models.DurationField(blank=True)
+    activity = models.CharField(max_length=200)
     
     def __str__(self):
-       return self.description
-       
-    def duration(self):
-       return end_time-start_time
+       return self.activity
+      
--- mysite/stopwatch/views.py
+++ mysite/stopwatch/views.py
@@ -2,14 +2,20 @@ from django.http import HttpResponse
 from django.template import loader
 from .models import Record
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def index(request):
     # '-' indicates a descending order
-    records = Record.objects.order_by('-record_date')[:5]
+    #records = Record.objects.order_by('-start_time')[:5]
+    records = Record.objects.filter(user=request.user)
     template = loader.get_template('stopwatch/index.html')
     context = {
         'records':records,
     }
     return HttpResponse(template.render(context, request))
     
-def chart(request, user_id):
-    return HttpResponse("There will be a chart for user %s" % user_id)
+def chart(request, id):
+    return HttpResponse("Details for record %s" % id)
+    
+   
