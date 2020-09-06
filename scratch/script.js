c54d90485c64794eb0502a94f7b28cb58c2648b1
Maksym <maximka3d@gmail.com>
2020-09-06 01:29:49 -0500

    Added a progress bar
    - this will be used to visually differentiate different records
    - still requires correct current & max values... (todo next)

--- scratch/script.js
+++ scratch/script.js
@@ -1,12 +1,11 @@
 // state of a clock
-let ms = 0
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
-let clockRunning = false; 
-
-
+var ms = 0
+var seconds = 0;
+var minutes = 0;
+var hours = 0;
+var clockRunning = false; 
 var today = new Date();
+
   
 function stopWatch(){
 	ms++;
@@ -61,6 +60,7 @@ function resetClock(){
 		seconds = 0;
 		minutes = 0;
 		hours = 0;
+		today = new Date();
 	}
 	
 	document.getElementById("clock").innerHTML = "0s:0";
@@ -80,12 +80,14 @@ function addRow() {
   var activity = document.getElementById("question");
   
   var row = table.insertRow(1);
+
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
   var cell4 = row.insertCell(3);
-  
-  // cel 3
+  var cell5 = row.insertCell(4);
+
+  // Duration
   var clock_time = document.getElementById("clock").innerHTML;
   var time_input = document.createElement("input"); 
   time_input.className = 'record_entry';  
@@ -93,7 +95,7 @@ function addRow() {
   time_input.value = clock_time;  
   cell3.appendChild(time_input);
   
-  // cell 1
+  // Activity name
   var rowIndex = table.getElementsByTagName("tr").length;
   var activity_text = "";
   if(activity.value == ""){
@@ -105,22 +107,62 @@ function addRow() {
   activity_input.value = activity_text;
   cell1.appendChild(activity_input);
   
-  // cell 4
+  // Delete button
   var delete_btn = document.createElement("input"); 
   delete_btn.type ='button';  
-  delete_btn.value = 'Delete';  
+  delete_btn.value = '⨉';  
   delete_btn.onclick=function(){deleteRow(this)};
-  cell4.appendChild(delete_btn);  
+  cell5.appendChild(delete_btn);  
 
-  // cell 2
-  //<td><input class="record_entry" type="time" value="14:03"></td>
+  // Start time
   var start_time = document.createElement("input"); 
   start_time.className = 'record_entry'; 
   start_time.type ='time';  
-
-  // adding leading zeroes
   var h = today.getHours() < 10 ? h ="0"+today.getHours() : h = today.getHours();
   var m = today.getMinutes() < 10 ? m ="0"+today.getMinutes() : m = today.getMinutes();  
   start_time.value = h+':'+m;
   cell2.appendChild(start_time);  
+
+  // Progress bar
+  var svg = d3.create("svg");
+
+  svg
+  .attr("width", 100)
+  .attr("height", 12);
+
+  setProgress(svg, Math.floor(Math.random() * 101), 100); // @TODO
+  d3.select(cell4).append(() => svg.node());
 }
+
+
+
+
+/// Progress bar
+
+function setProgress(svg, curr, max){
+
+  var w = svg.attr("width");
+  var h = svg.attr("height");
+
+  var background = svg.selectAll(null)
+	.data([curr])
+	.enter()
+	.append("rect")
+	.attr("height", h)
+	.attr("width", w)
+	.style("fill", "#222222");
+  
+  var progress = svg.selectAll(null)
+	.data([max])
+	.enter()
+	.append("rect")
+	.attr("height", h)
+	.attr("width", curr/max*w)
+	.style("fill", "#5A6BFF");
+}
+
+  var svg = d3.select("svg");
+   setProgress(svg, 12, 100)
+  
+
+  
