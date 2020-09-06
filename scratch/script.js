c5912fd6f7e4c0322f463956582996c055e07a32
Maksym <maximka3d@gmail.com>
2020-09-06 04:26:59 -0500

    Can now parse JSON to build a table of records

--- scratch/script.js
+++ scratch/script.js
@@ -1,4 +1,5 @@
-// state of a clock
+// Clock
+
 var ms = 0
 var seconds = 0;
 var minutes = 0;
@@ -6,6 +7,30 @@ var hours = 0;
 var clockRunning = false; 
 var today = new Date();
 
+function timeToSeconds(hhmmss){
+	var time = hhmmss.split(':');
+	var seconds = (time[0]) * 60 * 60 + (time[1]) * 60 + (time[2]);  // minutes are worth 60 seconds. Hours are worth 60 minutes.
+	return seconds;
+}
+
+function currentTime(){ // hh:mm:ss format
+	var time = "";
+	if(hours == 0) {time = time +'00:';}
+	else if(hours<10) {	time = time +'0'+hours+":";}
+	else{time = time+hours+":";}
+
+
+
+	if(minutes == 0) {time = time +'00:';}
+	else if(minutes<10) {time = time +'0'+minutes+":";}
+	else{time = time+minutes+":";}
+
+	if(seconds == 0) {time = time +'00';}
+	else if(seconds<10) {time = time +'0'+seconds;}
+	else{time = time+seconds;}
+
+	return time;
+}
   
 function stopWatch(){
 	ms++;
@@ -42,12 +67,15 @@ function startClock(){
 		thread = window.setInterval(stopWatch,100);
 		document.getElementById("start_btn").innerHTML = "PAUSE";
 		document.getElementById("clock").style.color = "#FFFFFF";
-		//document.getElementById("save_btn").style.display = "none";
+		document.getElementById("reset_btn").classList.add("disabled");
+		document.getElementById("save_btn").classList.add("disabled");		
 	}
 	else{
 		window.clearInterval(thread);
 		document.getElementById("clock").style.color = "#666666";
 		document.getElementById("start_btn").innerHTML = "CONTINUE";
+		document.getElementById("reset_btn").classList.remove("disabled");
+		document.getElementById("save_btn").classList.remove("disabled");	
 	}	
 	clockRunning = !clockRunning;
 }	
@@ -62,90 +90,115 @@ function resetClock(){
 		hours = 0;
 		today = new Date();
 	}
-	
 	document.getElementById("clock").innerHTML = "0s:0";
 	document.getElementById("clock").style.color = "#666666";
 	document.getElementById("start_btn").innerHTML = "START";
+	document.getElementById("reset_btn").classList.add("disabled");
+	document.getElementById("save_btn").classList.add("disabled");	
 }	
 
 function deleteRow(r) {
   //console.log("delete pressed");
+
+  var records_table = document.getElementById("records_table");
   var i = r.parentNode.parentNode.rowIndex;
-  document.getElementById("records_table").deleteRow(i);
+  records_table.deleteRow(i);
+  if(records_table.rows.length==1){
+	records_table.style.visibility = "hidden";
+  }
 }
 
 
-function addRow() {
-  var table = document.getElementById("records_table");
-  var activity = document.getElementById("question");
-  
-  var row = table.insertRow(1);
+function createRow() {
 
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  var cell3 = row.insertCell(2);
-  var cell4 = row.insertCell(3);
-  var cell5 = row.insertCell(4);
+  var duration_time = currentTime();
 
-  // Duration
-  var clock_time = document.getElementById("clock").innerHTML;
-  var time_input = document.createElement("input"); 
-  time_input.className = 'record_entry';  
-  time_input.style.type ='text';  
-  time_input.value = clock_time;  
-  cell3.appendChild(time_input);
-  
-  // Activity name
-  var rowIndex = table.getElementsByTagName("tr").length;
-  var activity_text = "";
-  if(activity.value == ""){
-	 activity_text = "Activity "+rowIndex; 
-  }else{
-	 activity_text = activity.value;
-  }  
-  var activity_input = time_input.cloneNode(true);  
-  activity_input.value = activity_text;
-  cell1.appendChild(activity_input);
-  
-  // Delete button
-  var delete_btn = document.createElement("input"); 
-  delete_btn.type ='button';  
-  delete_btn.value = '⨉';  
-  delete_btn.onclick=function(){deleteRow(this)};
-  cell5.appendChild(delete_btn);  
-
-  // Start time
-  var start_time = document.createElement("input"); 
-  start_time.className = 'record_entry'; 
-  start_time.type ='time';  
   var h = today.getHours() < 10 ? h ="0"+today.getHours() : h = today.getHours();
   var m = today.getMinutes() < 10 ? m ="0"+today.getMinutes() : m = today.getMinutes();  
-  start_time.value = h+':'+m;
-  cell2.appendChild(start_time);  
+  var start_time  = h+':'+m;
 
-  // Progress bar
-  var svg = d3.create("svg");
-
-  svg
-  .attr("width", 100)
-  .attr("height", 12);
-
-  setProgress(svg, Math.floor(Math.random() * 101), 100); // @TODO
-  d3.select(cell4).append(() => svg.node());
+  var table = document.getElementById("records_table");
+  var activity = document.getElementById("question");
+  var rowIndex = table.rows.length;
+  var activity_name = "";
+  if(activity.value == ""){
+	activity_name = "Activity "+(rowIndex-1); 
+  }else{
+	activity_name = activity.value;
+  }  
+  
+  var seconds = timeToSeconds(duration_time);
+  if(seconds<longestTime){
+	addRow(activity_name, start_time, duration_time, seconds/longestTime);
+  }
+  resetClock();
 }
+
+
+function addRow(activity_name, start_time, duration, progress_percentage) {
+	var table = document.getElementById("records_table");
+	
+	if(table.rows.length==1){
+	  records_table.style.visibility = "visible";
+	}
+  
+	var row = table.insertRow(1);
+  
+	var cell1 = row.insertCell(0);
+	var cell2 = row.insertCell(1);
+	var cell3 = row.insertCell(2);
+	var cell4 = row.insertCell(3);
+	var cell5 = row.insertCell(4);
+  
+	// Duration
+	var duration_input = document.createElement("input"); 
+	duration_input.className = 'record_entry';  
+	duration_input.style.type ='text';  
+	duration_input.value = duration;  
+	cell3.appendChild(duration_input);
+	
+	// Activity name
+	var activity_input = duration_input.cloneNode(true);  
+	activity_input.value = activity_name;
+	cell1.appendChild(activity_input);
+	
+	// Delete button
+	var delete_btn = document.createElement("input"); 
+	delete_btn.type ='button';  
+	delete_btn.value = 'X';  
+	delete_btn.onclick=function(){deleteRow(this)};
+	cell5.appendChild(delete_btn);  
+  
+	// Start time
+	var start_input = document.createElement("input"); 
+	start_input.className = 'record_entry'; 
+	start_input.type ='time';  
+	start_input.value = start_time;
+	cell2.appendChild(start_input);  
+  
+	// Progress bar
+	var svg = d3.create("svg");
+  
+	svg
+	.attr("width", 100)
+	.attr("height", 12);
+  
+	setProgress(svg, progress_percentage);
+	d3.select(cell4).append(() => svg.node());
+  }
 
 
 
 
 /// Progress bar
 
-function setProgress(svg, curr, max){
+function setProgress(svg, value){
 
   var w = svg.attr("width");
   var h = svg.attr("height");
 
   var background = svg.selectAll(null)
-	.data([curr])
+	.data([value*w])
 	.enter()
 	.append("rect")
 	.attr("height", h)
@@ -153,16 +206,45 @@ function setProgress(svg, curr, max){
 	.style("fill", "#222222");
   
   var progress = svg.selectAll(null)
-	.data([max])
+	.data([w])
 	.enter()
 	.append("rect")
 	.attr("height", h)
-	.attr("width", curr/max*w)
+	.attr("width", value*w)
 	.style("fill", "#5A6BFF");
 }
 
-  var svg = d3.select("svg");
-   setProgress(svg, 12, 100)
+// var svg = d3.select("svg");
+// setProgress(svg, 12, 100)
   
 
-  
+
+/////
+
+var response = '{ "records" : [' +
+'{ "activity":"Programming" , "start":"13:30", "duration":"00:00:20" },' +
+'{ "activity":"Gym" , "start":"10:00", "duration":"00:00:40" }]}';
+
+var obj = JSON.parse(response);
+var records = obj.records;
+var durations = [];
+
+
+// find longest time
+var longestTime = 0;
+
+records.forEach(function (item) {
+	var curr = timeToSeconds(item.duration);
+	//console.log(curr);
+	durations.push(curr);
+	if(longestTime < curr){
+		longestTime = curr;
+	}
+  });
+//console.log(longestTime);
+
+// add rows to the table
+records.forEach(function (item, index) {
+	var progress = durations[index]/longestTime;
+	addRow(item.activity,item.start,item.duration, progress);
+  });
